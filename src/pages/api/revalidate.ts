c51202de77a1bce4e830d026772f408342841e9b
Luiz <luizmon.silva@gmail.com>
2023-04-25 12:40:32 -0300

    deploy de revalidação

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let inboundRevalToken = req.headers["x-vercel-reval-key"];
+  if (!inboundRevalToken) {
+    return res
+      .status(401)
+      .json({ message: "x-vercel-reval-key header not defined" });
+  } else if (inboundRevalToken !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    let postSlug = req.body.fields.slug["pt-BR"];
+
+
+    await res.revalidate(`/posts/${postSlug}`);
+    await res.revalidate("/");
+
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}
